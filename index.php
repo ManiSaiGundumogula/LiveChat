922fa15d773fcb54869de340a9377924986c721a
Mani Sai Gundumogula <mani4sai@gmail.com>
2019-10-14 16:38:08 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ p    {color: red;}
 </head>
 <body>
 <center>
-<h1>My First Step</h1>
-<p>Hey here in this page you can chat live with Anil Kumar</p>
+<h1>My First Live Chat Application</h1>
+<p>Hey here in this page you can chat live with Mani Sai</p>
 </center>
 <div id="liveagent_invite_button_5737F000000keX7" style="display:none;"><span style="cursor: pointer; width: 100px; height: 20px; padding: 5px;" onclick="liveagent.startChat('5737F000000keX7')">Start Chat</span><span style="cursor: pointer; width: 100px; height: 20px; padding: 5px;" onclick="liveagent.rejectChat('5737F000000keX7')">Reject Chat</span></div>
 <script type='text/javascript' src='https://c.la1-c1-hnd.salesforceliveagent.com/content/g/js/47.0/deployment.js'></script>
