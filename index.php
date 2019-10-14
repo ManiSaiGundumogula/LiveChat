3ebfd1a1788575d6b140df738aa1acd69edce97d
Mani Sai Gundumogula <mani4sai@gmail.com>
2019-10-14 16:30:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<HTML>
+
+<head>
+<style>
+body {background-color: powderblue;}
+h1   {color: blue;}
+p    {color: red;}
+</style>
+</head>
+<body>
+<center>
+<h1>My First Step</h1>
+<p>Hey here in this page you can chat live with Anil Kumar</p>
+</center>
+<div id="liveagent_invite_button_5737F000000keX7" style="display:none;"><span style="cursor: pointer; width: 100px; height: 20px; padding: 5px;" onclick="liveagent.startChat('5737F000000keX7')">Start Chat</span><span style="cursor: pointer; width: 100px; height: 20px; padding: 5px;" onclick="liveagent.rejectChat('5737F000000keX7')">Reject Chat</span></div>
+<script type='text/javascript' src='https://c.la1-c1-hnd.salesforceliveagent.com/content/g/js/47.0/deployment.js'></script>
+<script type='text/javascript'>
+liveagent.init('https://d.la1-c1-hnd.salesforceliveagent.com/chat', '5727F000000klDA', '00D7F000007DMvZ');
+</script>
+
+</BODY>
+</HTML>
+
